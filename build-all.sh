e6612388a1bce85f5c076a1c7d611a2957cbdec9
Ben Gamari <ben@smart-cactus.org>
2017-10-30 17:57:37 -0400

    Bump up maximum size

--- build-all.sh
+++ build-all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -ex
 
-args="-O1 -fsimpl-tick-factor=1000 -v -dverbose-core2core -ddump-to-file -dsuppress-uniques -dsuppress-ticks -dsuppress-idinfo"
-for i in 1 2 3 4 5 6 7; do 
-    ./build.sh $i $args
+args="-O1 -fsimpl-tick-factor=1000 -v -dverbose-core2core -ddump-to-file -dsuppress-ticks -ddump-simpl-stats -dsuppress-idinfo -dsuppress-coercions"
+export ghc=$HOME/ghc/ghc/inplace/bin/ghc-stage2
+for i in 1 2 3 4 5 6 7 8 9 10; do 
+    ./build.sh $i $args $@
     rm -Rf dump-$i/src/Runner.verbose-core2core.split || true
     ~/ghc-utils/split-core2core.py dump-$i/src/Runner.verbose-core2core
 done
--- build.sh
+++ build.sh
@@ -5,4 +5,8 @@ shift
 rm -f src/Runner.o src/Runner.hi
 cpp -DN=$n src/Runner.hs.in > src/Runner.hs
 mkdir -p dump-$n
-ghc -isrc Runner -XTypeOperators -XKindSignatures -XDataKinds -XFlexibleInstances -XConstraintKinds -XFlexibleContexts -XMultiParamTypeClasses -XTypeFamilies -dumpdir dump-$n $@ 2>&1 | tee dump-$n/log
+$ghc -isrc Runner \
+	-XTypeOperators -XKindSignatures -XDataKinds -XFlexibleInstances -XConstraintKinds \
+	-XFlexibleContexts -XMultiParamTypeClasses -XTypeFamilies \
+	-dumpdir dump-$n $@ >| dump-$n/log 2>&1
+
