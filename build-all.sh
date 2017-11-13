8a0000640a3361a4ce9e4643be2de02a88b5e9c2
Denis Redozubov <denis.redozubov@gmail.com>
2017-11-13 17:20:21 +0300

    OS X fixes and other

--- build-all.sh
+++ build-all.sh
@@ -2,9 +2,8 @@
 
 args="-O1 -fsimpl-tick-factor=1000 -v -dverbose-core2core -ddump-to-file -dsuppress-ticks -ddump-simpl-stats -dsuppress-idinfo -dsuppress-coercions"
 
-for i in 1 2 3 4 5 6 7 8 9 10; do 
+for i in 1 2 3 4 5 6 7 8 9 10; do
     n=$i ./build.sh $args $@
     rm -Rf dump-$i/src/Runner.verbose-core2core.split || true
-    ~/ghc-utils/split-core2core.py dump-$i/src/Runner.verbose-core2core
+    wget https://raw.githubusercontent.com/dredozubov/ghc-utils/python-fixes/split-core2core.py && ./split-core2core.py dump-$i/src/Runner.verbose-core2core
 done
-
--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+set -o xtrace
+
 if [ -z "$n" ]; then
 	echo "Please set \$n"
 	exit 1
@@ -8,8 +10,8 @@ fi
 rm -f src/Runner.o src/Runner.hi
 cpp -DN=$n src/Runner.hs.in > src/Runner.hs
 mkdir -p dump-$n
+sed -i '/^#/d' src/Runner.hs
 $ghc -isrc Runner \
 	-XTypeOperators -XKindSignatures -XDataKinds -XFlexibleInstances -XConstraintKinds \
 	-XFlexibleContexts -XMultiParamTypeClasses -XTypeFamilies \
 	-dumpdir dump-$n $@ >| dump-$n/log 2>&1
-
